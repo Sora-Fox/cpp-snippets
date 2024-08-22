c32495bab4df50d6897141900963014975f948ba
Fox <dhhugtu2@gmail.com>
2024-08-22 19:16:26 +0300

    timer.hpp updated
    * Added a default constructor that automatically starts the timer upon initialization.
    * Introduced a stopped boolean flag to track if the timer has been stopped.
    * Modified start() and stop() methods to update the stopped flag.
    * Refactored elapsed() method to calculate elapsed time correctly if the timer is still running.
    * Ensured backward compatibility with previous API by retaining the same interface for existing methods.

--- data-structures/timer.hpp
+++ data-structures/timer.hpp
@@ -1,32 +1,48 @@
 #include <chrono>
 #include <ratio>
 
-
 class Timer
 {
 public:
     enum class Unit { NANO, MICRO, MILLI, SEC, MIN };
-    void start() { this->begin = std::chrono::steady_clock::now(); }
-    void stop() { this->end = std::chrono::steady_clock::now(); }
+    Timer() { this->start(); }
+    void start(); 
+    void stop(); 
     double elapsed(Timer::Unit) const;
 private:
     std::chrono::time_point<std::chrono::steady_clock> begin, end;
+    bool stopped;
 };
+
+void Timer::start()
+{
+    this->stopped = false;
+    this->begin = std::chrono::steady_clock::now();
+}
+
+void Timer::stop()
+{
+    this->end = std::chrono::steady_clock::now(); 
+    this->stopped = true;
+}
 
 double Timer::elapsed(Timer::Unit unit=Timer::Unit::MILLI) const
 {
+    std::chrono::time_point<std::chrono::steady_clock> end;
+    end = this->stopped ? this->end : std::chrono::steady_clock::now();
+
     switch (unit)
     {
         case Timer::Unit::NANO:
-            return std::chrono::duration_cast<std::chrono::duration<double, std::nano>>(this->end - this->begin).count();
+            return std::chrono::duration_cast<std::chrono::duration<double, std::nano>>(end - this->begin).count();
         case Timer::Unit::MICRO:
-            return std::chrono::duration_cast<std::chrono::duration<double, std::micro>>(this->end - this->begin).count();
+            return std::chrono::duration_cast<std::chrono::duration<double, std::micro>>(end - this->begin).count();
         case Timer::Unit::MILLI:
-            return std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(this->end - this->begin).count();
+            return std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(end - this->begin).count();
         case Timer::Unit::SEC:
-            return std::chrono::duration_cast<std::chrono::duration<double>>(this->end - this->begin).count();
+            return std::chrono::duration_cast<std::chrono::duration<double>>(end - this->begin).count();
         case Timer::Unit::MIN:
-            return std::chrono::duration_cast<std::chrono::duration<double, std::ratio<60>>>(this->end - this->begin).count();
+            return std::chrono::duration_cast<std::chrono::duration<double, std::ratio<60>>>(end - this->begin).count();
         default:
             return 0.0;
     }
