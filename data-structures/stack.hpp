cf782e09e7323ee61a3cec514b241a911e650f0c
Fox <dhhugtu2@gmail.com>
2024-08-24 11:19:25 +0300

    Stack.hpp created

---
+++ data-structures/stack.hpp
@@ -0,0 +1,86 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+
+#include <stdexcept>
+
+
+template <typename T>
+class Stack
+{
+public:
+    Stack() : peek(nullptr), length(0) {};
+    ~Stack();
+
+    Stack(const Stack&) = delete;
+    void operator=(const Stack&) = delete;
+
+    void push(const T&);
+    void pop();
+    T top() const;
+
+    bool empty() const { return this->length == 0; }
+    size_t size() const { return this->length; }
+
+private:
+    struct Node;
+    Node* peek;
+    size_t length;
+
+    void throwIfEmpty() const; 
+
+    struct Node
+    {
+        explicit Node(const T& data, const Node* prev) : data(data), prev(prev) {};
+
+        T data;
+        Node* prev;
+    };
+        
+};
+
+
+template <typename T>
+Stack<T>::~Stack()
+{
+    while (this->peek != nullptr)
+    {
+        Node* new_peek = this->peek->prev;
+        delete this->peek;
+        this->peek = new_peek;
+    }
+}
+
+template <typename T>
+T Stack<T>::top() const 
+{
+    this->throwIfEmpty();
+    return this->peek->data;
+}
+template <typename T>
+void Stack<T>::push(const T& data)
+{
+    this->peek = new Node(data, this->peek);
+    ++this->length;
+}
+
+template <typename T>
+void Stack<T>::pop() 
+{
+    this->throwIfEmpty();
+
+    Node* new_peek = this->peek->prev;
+    delete this->peek;
+    this->peek = new_peek;
+
+    --this->length;
+}
+
+template <typename T>
+void Stack<T>::throwIfEmpty() const
+{
+    if (this->length == 0)
+        throw std::runtime_error("Stack is empty.");
+}
+
+
+#endif // STACK_HPP
